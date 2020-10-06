2c19adaa931245579746a6d73ff0295afcafa5fc
Tadashi Ringo <phamt8611@gmail.com>
2020-10-06 13:30:59 +0700

    Thử nghiệm

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Chào mừng bạn đến với YuriNovel"
+    return "Chào mừng bạn đến với YuriNovel 2020"
 def words():
     return "YuriNovel cung cấp tiểu thuyết bách hợp miễn phí"
