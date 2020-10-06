b7e7dfe9514d73b9a4d97d0f022dddbd47f55dac
Tadashi Ringo <phamt8611@gmail.com>
2020-10-06 14:26:04 +0700

    Fix

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def hello():
     return "Chào mừng bạn đến với YuriNovel 1"
-@app.route("/test")
+
+
 
 if __name__=="__main__":
     app.run()
