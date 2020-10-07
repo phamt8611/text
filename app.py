eda7e8ec346fcb66c906bc4a9e93195d43c010a0
Tadashi Ringo <phamt8611@gmail.com>
2020-10-07 07:06:19 +0700

    Thêm màu cho tiêu đề

--- app.py
+++ app.py
@@ -10,14 +10,25 @@ Data = {
         "Content":"Nền tảng văn học mạng, cung cấp truyện bách hợp miễn phí, gồm các thể loại đồng nhân, đô thị, nhị thứ nguyên, tam thứ nguyên",
         "Author":"Tadashi Ringo",
         "Charset":"UTF-8",
-        "Keywords":"truyện bách hợp, truyện yuri, Yuri, Novel, Tadashi Ringo, Lesbian"
+        "Keywords":"truyện bách hợp, truyện yuri, Yuri, Novel, Tadashi Ringo, Lesbian",
+        "Color":{
+            "Title":"color:red"
+            }
         }
 
 
 
 @app.route("/")
 def index():
-    return render_template("index.html",Title=Data['Title'],Description=Data['Content'],Keywords=Data['Keywords'],Charset=Data['Charset'],Author=Data['Author'])
+    return render_template(
+            "index.html",
+            Title=Data['Title'],
+            Description=Data['Content'],
+            Keywords=Data['Keywords'],
+            Charset=Data['Charset'],
+            Author=Data['Author'],
+            ColorTitle=Data['Color']['Title']
+            )
 
 
 
